9dfb78cd1069d0e8d30fbfa0c88d832cd1621dc3
Benjamin Lacroix <benjamin.lacroix@publicissapient.com>
2021-01-18 20:29:04 +0100

    💄 font + header

---
+++ src/pages/index.tsx
@@ -0,0 +1,12 @@
+import * as React from 'react';
+import '../styles.scss';
+import { Header } from '../components/Header';
+
+export default function IndexRoute() {
+  return (
+    <>
+      <Header/>
+
+    </>
+  );
+}

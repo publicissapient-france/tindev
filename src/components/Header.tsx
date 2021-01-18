d3b624c040251e1f93925277328b5141f470b4c0
Benjamin Lacroix <benjamin.lacroix@publicissapient.com>
2021-01-18 22:08:03 +0100

    :sparkles: card

--- src/components/Header.tsx
+++
@@ -1,9 +0,0 @@
-import * as React from 'react';
-import styles from './Header.module.scss';
-import logo from '../images/logo.svg';
-
-export const Header = () =>
-  <div className={styles.Header}>
-    <img className={styles.logo} src={logo} alt={logo}/>
-    <div className={styles.tagline}>be the next match</div>
-  </div>;

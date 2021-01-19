c4f438fc5b501bdf79602288256325673b188c1d
Benjamin Lacroix <benjamin.lacroix@publicissapient.com>
2021-01-19 21:22:27 +0100

    :sparkles: card

---
+++ src/pages/play.tsx
@@ -0,0 +1,12 @@
+import * as React from 'react';
+import { Header2 } from '../components/Header/Header2';
+import { Card } from '../components/Card/Card';
+
+export default function () {
+  return (
+    <>
+      <Header2/>
+      <Card/>
+    </>
+  );
+}

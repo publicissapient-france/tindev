72ccc4f39686288dd1e411cb863a41fb269c039a
Stéphane Francel <contact@avine.io>
2021-01-19 11:32:55 +0100

    Fix `buildDataset` (map items)

--- src/data/utils/buildDataset.js
+++ src/data/utils/buildDataset.js
@@ -1,13 +1,19 @@
 import shuffle from "./shuffle";
 
-const importantSize = 10
+const importantSize = 10;
 const funSize = 10;
+
+const mapItem = ({ question, important, response }) => ({
+  question,
+  important: important === "OUI",
+  response: response === "❤",
+});
 
 export default (data) => {
   const important = [];
   const fun = [];
 
-  data.forEach((item) => (item.important ? important : fun).push(item));
+  data.forEach((item) => (item.important ? important : fun).push(mapItem(item)));
 
   return shuffle([
     ...shuffle(important).slice(0, importantSize),

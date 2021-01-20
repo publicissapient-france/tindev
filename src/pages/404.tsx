c2d601da076a5b0fba9063396d094c009e5cf53e
Benjamin Lacroix <benjamin.lacroix@publicissapient.com>
2021-01-20 19:42:00 +0100

    :bug: fix 404 (#9)

--- src/pages/404.tsx
+++ src/pages/404.tsx
@@ -1,5 +1,13 @@
+import { useEffect } from 'react';
 import { navigate } from 'gatsby';
+import * as React from 'react';
 
-export default function () {
-  return navigate('/');
+export default function NotFoundPage() {
+  useEffect(() => {
+    navigate('/'); // redirecting to home page
+  }, []);
+
+  return (
+    <h1>(404) NotFound Page</h1>
+  );
 }

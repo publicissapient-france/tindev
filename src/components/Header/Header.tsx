f6b5fcc50027a71eb9db25a24b1ad1708ec0480f
Stéphane Francel <contact@avine.io>
2021-01-20 12:43:30 +0100

    Responsive Header with/without background

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,9 +1,12 @@
 import * as React from 'react';
 import styles from './Header.module.scss';
-import logo from '../../images/logo.svg';
+import logoWhite from '../../images/logo-white.svg';
+import logoBlack from '../../images/logo-black.svg';
 
-export const Header = () =>
-  <div className={styles.Header}>
-    <img className={styles.logo} src={logo} alt={logo}/>
-    <div className={styles.tagline}>be the next match</div>
-  </div>;
+export const Header = ({ background }: { background: boolean }) =>
+  <header className={`${styles.header} ${background ? styles.headerAlt : ''}`}>
+    <div className={styles.container}>
+      <img src={background ? logoWhite : logoBlack} alt='logo'/>
+      <div className={styles.tagline}>Be the next match</div>
+    </div>
+  </header>;

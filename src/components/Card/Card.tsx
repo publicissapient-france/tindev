96c4d0a8d88b582a9047d972c0c386709c191bed
Stéphane Francel <contact@avine.io>
2021-01-21 11:47:23 +0100

    Add <Layout> component and fix <Deck> CSS

--- src/components/Card/Card.tsx
+++ src/components/Card/Card.tsx
@@ -1,22 +1,23 @@
 import React from 'react';
-import cardStyles from './Card.module.scss';
-import image from '../../images/card-image.svg';
-import { Action, ActionType } from '../Action/Action';
 import { animated } from 'react-spring';
 import { ReactEventHandlers } from 'react-use-gesture/dist/types';
+
+import image from '../../images/card-image.svg';
+import { Action, ActionType } from '../Action/Action';
+import cardStyles from './Card.module.scss';
 
 type Props = { onYes: ReactEventHandlers, onNo: ReactEventHandlers };
 
 export const Card = ({ onYes, onNo }: Props) => (
-  <div className={cardStyles.Card}>
-    <div className={cardStyles.image} style={{ backgroundImage: `url(${image})` }}/>
+  <div className={cardStyles.card}>
+    <div className={cardStyles.image} style={{ backgroundImage: `url(${image})` }} />
     <p className={cardStyles.description}>la qualité sans compromis ?</p>
     <div className={cardStyles.actions}>
       <animated.div {...onNo}>
-        <Action type={ActionType.NO}/>
+        <Action type={ActionType.NO} />
       </animated.div>
       <animated.div {...onYes}>
-        <Action type={ActionType.YES}/>
+        <Action type={ActionType.YES} />
       </animated.div>
     </div>
   </div>
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,27 +1,24 @@
-import React from 'react';
-import '../styles.scss';
-import indexStyles from './index.module.scss';
-import { Header } from '../components/Header/Header';
-import { Button } from '../components/Button/Button';
 import { Link } from 'gatsby';
+import React from 'react';
 
-export default function IndexRoute() {
-  return (
+import { Button } from '../components/Button/Button';
+import { Header } from '../components/Header/Header';
+import Layout from '../components/Layout/Layout';
+import indexStyles from './index.module.scss';
+
+const IndexRoute = () =>
+  <Layout header={<Header background={true} />}>
     <div className={indexStyles.index}>
-      <div className={indexStyles.header}>
-        <Header background={true}/>
-      </div>
-      <main className={indexStyles.main}>
-        <div className={indexStyles.content}>
-          <div className={indexStyles.welcome}>
-            Découvrez si vous êtes<br/>notre prochain
-            <span className={indexStyles.welcomeBig}>match !</span>
-          </div>
-          <Link to={'/play'}>
-            <Button>c'est parti !</Button>
-          </Link>
+      <div className={indexStyles.content}>
+        <div className={indexStyles.welcome}>
+          Découvrez si vous êtes<br />notre prochain
+              <span className={indexStyles.welcomeBig}>match !</span>
         </div>
-      </main>
+        <Link to={'/play'}>
+          <Button>c'est parti !</Button>
+        </Link>
+      </div>
     </div>
-  );
-}
+  </Layout>
+
+export default IndexRoute;

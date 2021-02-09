84a9cf82534a8961300a9925c9f0e0873bffbec5
Benjamin Lacroix <benjamin.lacroix@publicissapient.com>
2021-02-09 17:13:21 +0100

    :recycle: code review

--- src/components/Modal/Modal.tsx
+++ src/components/Modal/Modal.tsx
@@ -1,8 +1,9 @@
 import React, { FunctionComponent } from 'react';
 import styles from './Modal.module.scss';
+import { navigate } from 'gatsby';
 
 export const Modal: FunctionComponent = ({ children }) =>
-  <div className={styles.background}>
+  <div className={styles.background} onClick={() => navigate('/')}>
     <div className={styles.head}/>
     <div className={styles.modal}>
       {children}
